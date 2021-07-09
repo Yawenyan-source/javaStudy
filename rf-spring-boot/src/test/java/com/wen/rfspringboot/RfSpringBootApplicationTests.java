c30d985b7718a6da30478c99471a01f44a0b80fe
wen <1785484015@qq.com>
2021-07-09 17:17:49 +0800

    java反射

---
+++ rf-spring-boot/src/test/java/com/wen/rfspringboot/RfSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wen.rfspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
