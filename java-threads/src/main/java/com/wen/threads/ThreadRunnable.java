cf18b6c94cf1684d51418d16542b1b51fdc747b2
wen <1785484015@qq.com>
2021-07-12 21:18:41 +0800

    创建多线程的几个方式

--- java-threads/src/main/java/com/wen/threads/ThreadRunnable.java
+++ java-threads/src/main/java/com/wen/threads/ThreadRunnable.java
@@ -1,7 +1,9 @@
 package com.wen.threads;
 
 /**
- * 方式二：实现Runnable接口
+ * 方式二：实现Runnable接口创建线程
+ * 使用匿名内部类的形式创建线程
+ * 使用lambda表达式创建线程
  */
 public class ThreadRunnable implements Runnable {
     @Override
@@ -12,15 +14,15 @@ public class ThreadRunnable implements Runnable {
     public static void main(String[] args) {
         //启动线程
 //        new Thread(new ThreadRunnable()).start();
-        new Thread(() -> System.out.println(Thread.currentThread().getName() + "我是子线程")).start();
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + "<我是子线程>")).start();
         //使用匿名内部类的形式来创建线程
         new Thread(() -> {
             try {
                 Thread.sleep(3000);
-                System.out.println(Thread.currentThread().getName() + "我也是子线程");
+                System.out.println(Thread.currentThread().getName() + "<我也是子线程>");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }).start();
+        }, "wen线程").start();
     }
 }
