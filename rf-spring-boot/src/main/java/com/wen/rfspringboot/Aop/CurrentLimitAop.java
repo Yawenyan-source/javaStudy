09646862962f90d8feafbf0e9ad80b0514c39435
wen <1785484015@qq.com>
2021-07-12 17:35:45 +0800

    java反射+自定义注解

--- rf-spring-boot/src/main/java/com/wen/rfspringboot/Aop/CurrentLimitAop.java
+++ rf-spring-boot/src/main/java/com/wen/rfspringboot/Aop/CurrentLimitAop.java
@@ -1,10 +1,15 @@
 package com.wen.rfspringboot.Aop;
 
 import com.google.common.util.concurrent.RateLimiter;
+import com.wen.rfspringboot.annotation.WenCurrentLimit;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @Aspect
 @Component
@@ -13,6 +18,7 @@ public class CurrentLimitAop {
      * 每秒生成1.0个令牌
      * */
     private RateLimiter rateLimiter = RateLimiter.create(1.0);
+    private ConcurrentHashMap<String, RateLimiter> rateLimiters = new ConcurrentHashMap<>();
 
     /**
      * 只要在方法上有加该自定义的限流注解，就会被aop环绕通知
@@ -23,6 +29,24 @@ public class CurrentLimitAop {
     @Around(value = "@annotation(com.wen.rfspringboot.annotation.WenCurrentLimit)")
     public Object around(ProceedingJoinPoint joinPoint) {
         try {
+            //获取拦截的方法名
+            Signature signature = joinPoint.getSignature();
+            //获取拦截的方法名
+            MethodSignature methodSignature = (MethodSignature) signature;
+            WenCurrentLimit wenCurrentLimit = methodSignature.getMethod().getDeclaredAnnotation(WenCurrentLimit.class);
+
+            //获取该注解的name
+            String name = wenCurrentLimit.name();
+            //通过注解上的name，获取到对应的rateLimiter
+            RateLimiter rateLimiter = rateLimiters.get(name);
+            //获取该注解的token
+            double token = wenCurrentLimit.token();
+            //如果没有对应的rateLimiter，则创建一个新的rateLimiter
+            if (rateLimiter == null) {
+                rateLimiter = RateLimiter.create(token);
+                //将这个rateLimiter保存起来
+                rateLimiters.put(name, rateLimiter);
+            }
             boolean b = rateLimiter.tryAcquire();
             if (!b) {
                 return "访问人数过多，请稍后重试";
--- rf-spring-boot/src/main/java/com/wen/rfspringboot/controller/TestController.java
+++ rf-spring-boot/src/main/java/com/wen/rfspringboot/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.wen.rfspringboot.controller;
 
-import com.google.common.util.concurrent.RateLimiter;
 import com.wen.rfspringboot.annotation.WenCurrentLimit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +10,11 @@ public class TestController {
     @GetMapping("/get")
     @WenCurrentLimit(name = "get", token = 1.0)
     public String get() {
-
         return "my is get";
     }
 
     @GetMapping("/add")
+    @WenCurrentLimit(name = "add", token = 2.0)
     public String add() {
         return "my is add";
     }
