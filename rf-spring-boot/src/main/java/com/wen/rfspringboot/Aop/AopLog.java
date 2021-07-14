849af4f08d60eb6e33857e308415723fa261e5f2
wen <1785484015@qq.com>
2021-07-15 00:13:32 +0800

    手写日志框架设计原理

---
+++ rf-spring-boot/src/main/java/com/wen/rfspringboot/Aop/AopLog.java
@@ -0,0 +1,61 @@
+package com.wen.rfspringboot.Aop;
+
+import com.alibaba.fastjson.JSON;
+import com.wen.rfspringboot.manage.LogManage;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+@Aspect
+@Component
+@Slf4j
+public class AopLog {
+    private static final String START_TIME = "request-start";
+    private SimpleDateFormat sdf4 = new SimpleDateFormat("yyyy年MM月dd日HH时mm分ss秒");
+    @Autowired
+    private LogManage logManage;
+
+    /**
+     * 切入点
+     */
+    @Pointcut("execution(public * com.wen.rfspringboot.controller.*Controller.*(..))")
+    public void log() {
+    }
+
+    /**
+     * 前置操作
+     *
+     * @param point 切入点
+     */
+    @Before("log()")
+    public void beforeLog(JoinPoint point) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+
+        logManage.info("【请求 时间】：" + sdf4.format(new Date()));
+        logManage.info("【请求 URL】：" + request.getRequestURL());
+        logManage.info("【请求 IP】：" + request.getRemoteAddr());
+        logManage.info("【类名 Class】：" + point.getSignature().getDeclaringTypeName());
+        logManage.info("【方法名 Method】：" + point.getSignature().getName());
+        logManage.info("【请求参数 Args】：" +
+                JSON.toJSONString(point.getArgs())
+        );
+//        log.info("【请求 时间】：{}", System.currentTimeMillis());
+//        log.info("【请求 URL】：{}", request.getRequestURL());
+//        log.info("【请求 IP】：{}", request.getRemoteAddr());
+//        log.info("【类名 Class】：{}，【方法名 Method】：{}", point.getSignature().getDeclaringTypeName(), point.getSignature().getName());
+//        log.info("【请求参数 Args】：{}，", point.getArgs());
+    }
+
+}
--- rf-spring-boot/src/main/java/com/wen/rfspringboot/manage/LogManage.java
+++ rf-spring-boot/src/main/java/com/wen/rfspringboot/manage/LogManage.java
@@ -1,4 +1,64 @@
 package com.wen.rfspringboot.manage;
 
+import com.wen.rfspringboot.utils.FileUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * 手写日框架
+ */
+@Component
 public class LogManage {
+    /**
+     * 定义一个缓存容器，缓存日志内容
+     */
+    private BlockingDeque<String> blockingDeque = new LinkedBlockingDeque<String>();
+    private LogThread logThread;
+    private String filePath = "/Users/wen/Desktop/temp/file.log";
+
+    public LogManage() {
+        logThread = new LogThread();
+        logThread.start();
+    }
+
+    public void info(String log) {
+        //将日志缓存起来
+        blockingDeque.offer(log);
+    }
+
+    class LogThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //缓冲池，批量写入到磁盘中
+                String log = blockingDeque.poll();
+                if (StringUtils.isEmpty(log)) {
+                    continue;
+                }
+                //继续获取消息
+                StringBuilder sbLog = new StringBuilder(log);
+                for (int i = 0; i < 9; i++) {
+                    String logTemp = blockingDeque.poll();
+                    if (StringUtils.isEmpty(logTemp)) {
+                        break;
+                    }
+                    sbLog.append(logTemp);
+                }
+                String logAppend = sbLog.toString();
+                if (!StringUtils.isEmpty(logAppend)) {
+                    //再将日志写入到磁盘中
+                    FileUtils.writeText(filePath, logAppend, true);
+                }
+            }
+        }
+    }
+
 }
